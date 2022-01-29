e9a640b7aed38d858bb23e4e6789d55edee356d9
Fabian H <fabian@faaren.com>
2022-01-29 22:27:57 +0100

    updated composer settings added ServiceProvider

---
+++ src/WebhookReceiverServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FaarenTech\WebhookReceiver;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebhookReceiverServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
